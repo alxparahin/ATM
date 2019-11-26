02a1fc0564d7f75d45ea94fb88dfcd5a7d226e57
alxparahin <Parakhin.A.Vla@sberbank.ru>
2019-11-26 21:51:05 +0300

    Exceptions+Collections

--- src/main/java/ru/sbrf/lesson/processing/Client.java
+++ src/main/java/ru/sbrf/lesson/processing/Client.java
@@ -1,12 +1,16 @@
 package ru.sbrf.lesson.processing;
 
+import java.util.ArrayList;
+
 public class Client {
     private int clientId;
     private int PIN;
+    private ArrayList account = new ArrayList();
 
-    public Client(int clientId, int PIN) {
+    public Client(int clientId, int PIN, Account account) {
         this.clientId = clientId;
         this.PIN = PIN;
+        this.account.add(account);
     }
 
     public int getClientId() {
@@ -16,4 +20,8 @@ public class Client {
     public int getPIN() {
         return PIN;
     }
+
+    public ArrayList getAccount() { return account; }
+
+    public Account getAccount(int accountId) { return (Account) account.get(accountId); }
 }
--- src/main/java/ru/sbrf/lesson/processing/Host.java
+++ src/main/java/ru/sbrf/lesson/processing/Host.java
@@ -6,29 +6,37 @@ import ru.sbrf.lesson.processing.exceptions.AccountNotFoundException;
 import ru.sbrf.lesson.processing.exceptions.ClientNotFoundException;
 import ru.sbrf.lesson.processing.exceptions.IncorrectPINException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Host {
-    private Client client;
-    private Account account;
+    //private Client client;
+    private HashMap client = new HashMap(1);
+    //private Account account;
+    private ArrayList accounts = new ArrayList(1);
 
     public Host(){
-        this(1,2,123,100);
+        this(1,0,123,100);
     }
 
     public Host(int clientId, int accountId, int PIN, int balance){
-        this.client = new Client(clientId,PIN);
-        this.account = new Account(accountId,clientId,balance);
+        client.put(clientId, new Client(clientId,PIN,new Account(accountId,clientId,balance)));
+        ///this.account = new Account(accountId,clientId,balance);
+        accounts.add(((Client) client.get(clientId)).getAccount());
+        //accounts.add(((Client) client.get(clientId)).getAccount());
     }
 
-    public Account getAccount() {
-        return account;
+    public Account getAccount(int accountId) {
+        return (Account) accounts.get(accountId);
     }
 
     public Response getBalance(Request request) {
-        if (request.getClientId() == client.getClientId()) {
-            if (request.getPIN() == client.getPIN()) {
-
-                if(request.getAccountId() == this.account.getAccountId()){
-                    return new Response(account.getBalance());
+        Client localClient = (Client) client.get(request.getClientId());
+        Account localAccount = localClient.getAccount(request.getAccountId());
+        if (request.getClientId() == localClient.getClientId()) {
+            if (request.getPIN() == localClient.getPIN()) {
+                if(request.getAccountId() == localAccount.getAccountId()) {
+                    return new Response(localAccount.getBalance());
                 } else throw new AccountNotFoundException("Счет не найден");
             } else throw new IncorrectPINException("ПИН неверный");
         } else throw new ClientNotFoundException("Клиент не найден");
